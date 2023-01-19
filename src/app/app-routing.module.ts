c83a5743fccfb2b8d1379031e90d4a18da0d8897
Max Gasaro <massimilianogasaro@me.com>
2023-01-19 17:33:59 +0100

    Create blog project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'homepage' },
+  {path: 'homepage', component: HomeComponent },
+  // {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard], children: [
+  //   {path: '', redirectTo: 'pagina1', pathMatch: 'full'},
+  //   {path: 'pagina1', component: Pagina1Component},
+  // ]
+  // },
+  // {path: 'login', component: LoginComponent},
+  // {path: 'register', component: RegisterComponent},
+  // {
+  //   path: '404',
+  //   component: NotFoundComponent
+  // },
+  // questo va sempre inserito come ultima path altrimenti rischiamo che prenda sempre questo
+  {
+    path: '**', // questo indica qualsiasi indirizzo diverso dai nostri
+    redirectTo: '/404'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
